b4dc24cfe49b17e57c12626510dcfd65add7946b
codyalmand <codybalmand@gmail.com>
2020-10-26 21:32:14 -0500

    Added quit option, add/show department functions

--- app.js
+++ app.js
@@ -6,9 +6,10 @@
 const path = require('path');
 const mysql = require('mysql');
 const inquirer = require('inquirer');
+const db = require(".");
 
 // create the connection information for the sql database
-var connection = mysql.createConnection({
+const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
@@ -23,38 +24,67 @@ connection.connect(function(err) {
     startApp();
   });
 
-//   After connection is made, the app starts and the user is prompted to add a department
+//   After connection is made, the app starts and the user is prompted to make a selection from options
  function startApp(){
     inquirer.prompt ([
       {
           name: "menu",
           type: "list",
-          message: "To scroll, use up/down arror keys, then use spacebar to select an option",
+          message: "Use up/down Arror Keys to scroll, select an option by using Enter",
           choices: [
             "View Departments",
             "Add Department",
             "View Employee Roles",
             "Add Employee Role",
             "Update Employee Role",
+            "Quit"
           ]
       }])
     .then(response => {
+      console.log(response);
         switch (response.menu) {
-          case "View Departments":
-            showDepartments();
+          case "View Departments": showDepartments();
             break;
-          case "Add Departments":
-            addDepartments();
+          case "Add Departments": addDepartment();
             break;
-          case "View Employee Roles":
-            viewRoles();
+          case "View Employee Roles": viewRoles();
             break;
-          case "Add Employee Role":
-            addRoles();
+          case "Add Employee Role": addRoles();
             break;
-          case "Update Employee Role":
-            updateRoles();
+          case "Update Employee Role": updateRoles();
             break;
-        }
+            case "Quit": quit();
+            break;
+        };
     }
+)};
+function quit(){ process.exit;
+  startApp();
+}
+
+// Builds a table which shows existing departments
+function showDepartments() {
+  console.log(' ');
+  const query = "SELECT * FROM department";
+  connection.query(query, (err, res) => {
+    if (err) throw err;
+    console.table(res);
+  });
+}
+
+
+// Add a new department to the database
+function addDepartment() {
+  inquirer.prompt([
+      {
+          name: "departmentName",
+          type: "input",
+          message: "Enter new department name:",
+      }
+  ]).then(response => {
+    connection.query('INSERT INTO department (name) VALUES (?)', [response.departmentName], (err, res) => {
+      if (err) throw err;
+      console.table(res);
+      }
+    )}
 )};
